3188f3c17aa3ca4177a83c3908c7a9994faab7f8
SH4RL1E <155279230+SH4RL1E@users.noreply.github.com>
2023-12-31 16:54:37 +0800

    Update intel.py

--- intel.py
+++ intel.py
@@ -416,7 +416,7 @@ if __name__ == "__main__":
                 print('\x1b[1;92m              ╚██████╗██║  ██║██║  ██║██║  ██║███████╗██║███████╗           \x1b[0m')
                 print('\x1b[1;92m               ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝           \x1b[0m')
                 print('\n\x1b[1;92m' + '-' * 50 + '\x1b[0m')
-                print('\n\x1b[1;92mOwner     :\x1b[0m \x1b[1;97mAiz\x1b[0m ')
+                print('\n\x1b[1;92mOwner     :\x1b[0m \x1b[1;97mCharlie\x1b[0m ')
                 print('\x1b[1;92mFacebook  :\x1b[0m \x1b[1;97mfacebook.com/charliesbb.74\x1b[0m ')
                 print('\x1b[1;92mTool Type :\x1b[0m \x1b[1;97mFacebook Manager\x1b[0m ')
                 print('\x1b[1;92mVersion   :\x1b[0m \x1b[1;97m1.0\x1b[0m ')
